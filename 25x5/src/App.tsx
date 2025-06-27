c45aec62b24ae01535838725969f4f9d91c9c39d
Heitor Hayashi <heitorhayashiferrairo@gmail.com>
2025-06-27 16:23:07 -0300

    feat: Heading module base done

--- 25x5/src/App.tsx
+++ 25x5/src/App.tsx
@@ -1,11 +1,20 @@
 import './styles/theme.css'
 import './styles/global.css'
 import { Heading } from './components/Heading';
+import { Hourglass } from 'lucide-react';
 
 export function Teste() {
     return (
         <>
-            <Heading />
+            <Heading>
+                    Olá mundo 
+                    <button> <Hourglass /> </button>
+            </Heading>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptate molestias iste repellat delectus impedit culpa ducimus dolores veritatis vel. Iusto,
+                voluptatem aliquid earum exercitationem repudiandae laboriosam incidunt voluptates dolorum a?
+            </p>
         </>
     );
 }
