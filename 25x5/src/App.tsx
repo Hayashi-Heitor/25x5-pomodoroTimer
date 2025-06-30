a192724a6a83158289d60d42924b95573163e92a
Heitor Hayashi <heitorhayashiferrairo@gmail.com>
2025-06-30 00:44:26 -0300

    feat: Menu icons added

--- 25x5/src/App.tsx
+++ 25x5/src/App.tsx
@@ -1,15 +1,19 @@
 import './styles/theme.css'
 import './styles/global.css'
 
-import { Heading } from './components/Heading';
 import { Container} from './components/Container'
 import { Logo } from './components/Logo'
+import { Menu } from './components/Menu'
 
 export function App() {
     return (
         <>
             <Container>
-                <Logo></Logo>
+                <Logo />
+            </Container>
+
+            <Container>
+                <Menu />
             </Container>
         </>
     );
