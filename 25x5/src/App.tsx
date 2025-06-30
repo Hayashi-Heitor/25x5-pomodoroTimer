6d3b7a3ac32de6f0dc7b9fcc85319778196560c8
Heitor Hayashi <heitorhayashiferrairo@gmail.com>
2025-06-30 12:54:26 -0300

    feat: Default Input component created

--- 25x5/src/App.tsx
+++ 25x5/src/App.tsx
@@ -5,6 +5,7 @@ import { Container} from './components/Container'
 import { Logo } from './components/Logo'
 import { Menu } from './components/Menu'
 import { CountDown } from './components/CountDown'
+import { DefaultInput } from './components/DefaultInput'
 
 export function App() {
     return (
@@ -19,6 +20,30 @@ export function App() {
 
             <Container>
                 <CountDown />
+            </Container>
+
+            <Container>
+                <form className='form' action=''>
+
+                    <div className='formRow'>
+                        <DefaultInput id='Input' type='text' labelText='task' placeholder='digite algo' />
+                    </div>
+                    
+                    <div className='formRow'>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+
+                    <div className='formRow'>
+                        <p>Ciclos</p>
+                        <p>0 0 0 0 0</p>
+                    </div>
+
+                    <div className='formRow'>
+                        <button>Enviar</button>
+                    </div>
+
+                </form>
+
             </Container>
         </>
     );
